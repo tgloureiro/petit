129af06aca7ea133d45db2fe1d001f1fcb703eca
Tiago Loureiro <tgloureiro@gmail.com>
2021-03-02 13:44:46 -0300

    feat: added adaptive app icon
    feat: added App scaffold and initial functions for screens

--- app/src/main/java/br/com/petit/MainActivity.kt
+++ app/src/main/java/br/com/petit/MainActivity.kt
@@ -3,36 +3,68 @@ package br.com.petit
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.petit.ui.theme.PetitTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            PetitTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            AppScaffold()
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AppScaffold() {
+    val white = Color(0xFFFFFFFF)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Petit") },
+                backgroundColor = white
+            )
+        },
+    ) {
+        MainScreen("World")
+    }
+}
+
+@Composable
+fun MainScreen(name: String) {
+    Text(
+        modifier = Modifier.padding(16.dp),
+        text = "Main screen: Hello $name!"
+    )
+}
+
+
+@Composable
+fun DetailsScreen(name: String) {
+    Text(
+        modifier = Modifier.padding(16.dp),
+        text = "Details screen: Hello $name!"
+    )
+}
+
+@Composable
+fun SuccessfulAdoptionScreen(name: String) {
+    Text(modifier = Modifier.padding(16.dp), text = "Successful Adoption screen: Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PetitTheme {
-        Greeting("Android")
+        AppScaffold()
     }
 }
+
